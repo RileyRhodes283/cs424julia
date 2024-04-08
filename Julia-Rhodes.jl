98c920d9b9a18cb7064be6001d56642abdf49c92
RileyRhodes283 <rhodesfam283@gmail.com>
2024-04-08 11:52:21 -0500

    Starting file

---
+++ Julia-Rhodes.jl
@@ -0,0 +1,21 @@
+#=
+File Name: Julia-Rhodes.jl
+Author: Riley Rhodes
+Description:
+Professor: Dr. Delugach 
+Use Instructions: 
+=#
+
+#=
+Class Name:
+
+=#
+#=
+Class Name:
+
+=#
+S
+#=
+Class Name:
+
+=#
