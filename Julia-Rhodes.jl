28ba296680fdf1f12ab7490e17a4ae382159ad50
Riley <rhodesfam283@gmail.com>
2024-04-09 16:06:42 -0500

    attempt before assignment is out

--- Julia-Rhodes.jl
+++ Julia-Rhodes.jl
@@ -8,14 +8,47 @@ Use Instructions:
 
 #=
 Class Name:
-
 =#
+holder = []
+struct Athlete 
+    name::String
+    ID::String
+end
+
+
+#=
+Class Name:
+=#
+struct Event
+    name::String
+    ID::String 
+end
 #=
 Class Name:
 
 =#
-S
-#=
-Class Name:
+#function Hi()
+    #print(open(io->read(io, String), "register.txt"))
 
-=#
+    open("register.txt") do line
+        while ! eof(line)
+        lines = readline(line)
+        if (lines == "")
+            continue
+        end
+        foo = split(lines,limit = 2)
+        #print("$foo\n")
+        ID::String = foo[1]
+        name::String  = foo[2]
+        print("ID = $ID\n")
+        print("Name = $name\n")
+        item = Athlete(ID,name)
+        print("$item\n")
+        #holder + item
+        
+        end 
+    end
+
+    #print("Holder = $holder\n")
+#end
+#Hi()
